58b29bb3f734c151c597d6d4604fbed28cc86b1b
Ben Swanson <bswan0002@gmail.com>
2020-10-13 17:46:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,8 +46,12 @@ def monopoly_with_fourth_tier
         four_pieces_owned: 200
       },
       names: {
-        reading_railroad: {},
-        pennsylvania_railroad: {},
+        reading_railroad: {
+          mortgage_value: 100
+        },
+        pennsylvania_railroad: {
+          mortgage_value: 
+        },
         b_and_o_railroad: {},
         shortline_railroad: {}
       }
