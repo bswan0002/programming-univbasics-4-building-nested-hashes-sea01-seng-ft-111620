2d4e028571dae922b501a00ce1f7bd20c4f7438b
Ben Swanson <bswan0002@gmail.com>
2020-10-13 17:41:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,12 @@ end
 
 def monopoly_with_second_tier
   monoTwoHash = {
-    :railroads => {
-      :pieces = 4
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
     }
+  }
 end
 
 def monopoly_with_third_tier
